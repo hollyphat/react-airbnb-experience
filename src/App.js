6602cee0aad3f73bc371c898c703dcc9d25f775e
Olatunji Fatai <sirhollyfat@gmail.com>
2022-02-26 08:32:14 +0100

    Use spread operator to pass data to card component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import Card from "./components/Card";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import data from "./data";
 
 function App() {
+  const cards = data.map(item => (
+    <Card 
+    key={item.id}
+    {...item}
+    />
+  ));
   return (
     <div className="App">
       <Navbar />
-      {/* <Hero /> */}
-      <Card
-        img="katie-zaferes.png"
-        rating="5.0"
-        reviewCount={6.0}
-        country="Austria"
-        title="Life lessons with Katie Zaferes"
-        price={136}
-       />
+      <Hero />    
+       <section className="cards-list">
+        { cards}
+       </section>
       
     </div>
   );
